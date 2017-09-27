3fd62015fc61217f531041b0c13a3ca06e282cff
Tapio Mattila <tapio.mattila@edu.turkuamk.fi>
2017-09-27 15:05:59 +0300

    Fixed jenkins checkstyle warnings in DateStorageModel class.

--- date-project/src/main/java/com/tmattila/model/DateStorageModel.java
+++ date-project/src/main/java/com/tmattila/model/DateStorageModel.java
@@ -16,35 +16,76 @@ public class DateStorageModel {
 	@Field(value = "DATE")
 	private String date;
 
+	/**
+	 * This is a comment for the DateStorageModel constructor.
+	 *
+	 * @param title
+	 *            ***is the title parameter***
+	 * @param date
+	 *            ***is the date parameter***
+	 */
 	public DateStorageModel(final String title, final String date) {
 		this.title = title;
 		this.date = date;
 	}
 
+	/**
+	 *
+	 * @return id
+	 */
 	public final String getId() {
 		return id;
 	}
 
+	/**
+	 * This is a comment for the setId setter method.
+	 *
+	 * @param id
+	 *            ***is the id parameter***
+	 */
 	public final void setId(final String id) {
 		this.id = id;
 	}
 
+	/**
+	 *
+	 * @return title
+	 */
 	public final String getTitle() {
 		return title;
 	}
 
+	/**
+	 * This is a comment for the setTitle setter method.
+	 *
+	 * @param title
+	 *            ***is the title parameter***
+	 */
 	public final void setTitle(final String title) {
 		this.title = title;
 	}
 
+	/**
+	 *
+	 * @return date
+	 */
 	public final String getDate() {
 		return date;
 	}
 
+	/**
+	 * This is a comment for the setDate setter method.
+	 *
+	 * @param date
+	 *            ***is the date parameter***
+	 */
 	public final void setDate(final String date) {
 		this.date = date;
 	}
 
+	/**
+	 * toString shows id, title and date in string format.
+	 */
 	@Override
 	public final String toString() {
 		return "ID: " + this.id + "\nTITLE: " + this.title + "\nDATE: " + this.date;
